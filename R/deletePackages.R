1e5f0bc6914540715913037c98a1f9ed4bd3957c
Anton Antonov <tonytonov@gmail.com>
2019-07-08 18:40:44 +0200

    Refactor deletion logic: prevent non-deps removal if this breaks dependencies; explicit params; more tests

--- R/deletePackages.R
+++ R/deletePackages.R
@@ -1,45 +1,55 @@
 #' Delete packages from a miniCRAN repository.
+#'
+#' Arguments \code{which}, \code{recursive} and \code{reverse} are passed over to
+#' \code{tools::package_dependencies} and allow to specify the deletion behaviour.
+#'
 #' @inheritParams addPackage
-#' @param deps logical indicating whether the package reverse dependencies should be removed (default `TRUE`).
-#' @param ... arguments passed over to `tools::package_dependencies`: use `which`, `recursive`, `reverse`
-#' to specify what should happen to dependencies of removed packages
+#' @param deps logical indicating whether the package reverse dependencies should
+#' be removed (default `TRUE`).
+#' @inheritParams tools::package_dependencies
 #' @export
 #' @family update repo functions
 deletePackage <- function(pkgs = NULL, path = NULL,
                           type = "source", Rversion = R.version,
-                          writePACKAGES = TRUE, deps = TRUE, ...) {
+                          writePACKAGES = TRUE, deps = TRUE,
+                          which = c("Depends", "Imports", "LinkingTo", "Suggests"),
+                          recursive = FALSE, reverse = FALSE) {
   if (is.null(path) || is.null(pkgs)) stop("path and pkgs must both be specified.")
 
   sapply(type, function(t) {
     repoPath <- file.path(path, repoPrefix(t, Rversion))
     db <- pkgAvail(repos = path, type = t, Rversion = Rversion)
-    purgePackage(pkgs, db, t, repoPath)
 
+    toRemove <- pkgs
     if (deps) {
-      d <- tools::package_dependencies(db = db, ...)
+      pdb <- tools::package_dependencies(db = db, which = which,
+                                         recursive = recursive,
+                                         reverse = reverse)
       depends <- unique(unlist(lapply(pkgs, function(pkg) {
-        if (!is.null(d[[pkg]])) d[[pkg]] else character()
+        if (!is.null(pdb[[pkg]])) pdb[[pkg]] else character()
       })))
-      l <- list(...)
-      if ('reverse' %in% names(l)) {
-        # Check for needed packages, which uses direct dependencies
-        l[['reverse']] <- FALSE
-        l[['db']] <- db
-        d <- do.call(tools::package_dependencies, l)
-      }
-      needed <- unique(unlist(d[!names(d) %in% c(pkgs, depends)]))
+      toRemove <- c(toRemove, depends)
+    }
 
-      toRemoveDeps <- depends[!depends %in% needed]
-      if (length(toRemoveDeps)) {
-        purgePackage(toRemoveDeps, db, t, repoPath)
-      }
+    # Checking if any of the packages in the removal list are required by other packages
+    pdb_needed <- tools::package_dependencies(
+      db = db,
+      which = c("Depends", "Imports", "LinkingTo", "Suggests"),
+      recursive = FALSE,
+      reverse = FALSE)
+
+    needed <- unique(unlist(pdb_needed[!names(pdb_needed) %in% toRemove]))
+    toPurge <- toRemove[!toRemove %in% needed]
+
+    if (length(toPurge)) {
+      purgePackage(toPurge, db, t, repoPath)
     }
   })
 
   if (writePACKAGES) invisible(updateRepoIndex(path = path, type = type, Rversion = Rversion))
 }
 
-# given packages, reconstruct file names and remove these files
+# Given a vector of package names, reconstruct file names and remove these files
 purgePackage <- function(pkgs, db, type, repoPath) {
   w <- which(db[, "Package"] %in% pkgs)
 
--- tests/testthat/test-8-deletePackage.R
+++ tests/testthat/test-8-deletePackage.R
@@ -29,32 +29,43 @@ context("deleteRepo")
 
 # LVGP imports randtoolbox, which depends on rngWELL
 # LVGP imports lhs, which imports Rcpp
-# RcppArmadillo imports Rcpp
-pkgsAdd1 <- "LVGP"
-pkgsAdd2 <- "RcppArmadillo"
-pkgsAdd <- c(pkgsAdd1, pkgsAdd2)
-pkgsDep1 <- c("randtoolbox", "lhs")
-pkgsDep2 <- "rngWELL"
-pkgsDep3 <- "Rcpp"
+# RcppArmadillo also imports Rcpp
+#
+#       RcppArmadillo
+#       /
+#   Rcpp -  lhs  -  LVGP
+#                    /
+# rngWELL - randoolbox
+
+pkg_lvgp <- "LVGP"
+pkg_rcpparma <- "RcppArmadillo"
+pkgsAdd <- c(pkg_lvgp, pkg_rcpparma)
+pkg_rtbox <- "randtoolbox"
+pkg_lhs <- "lhs"
+pkg_well <- "rngWELL"
+pkg_rcpp <- "Rcpp"
 
 mock_write_packages_delete <- function(dir, type = "source") {
   db <- structure(c(
-    "lhs", "LVGP", "randtoolbox", "Rcpp", "RcppArmadillo",
-    "rngWELL", "1.0.1", "2.1.5", "1.17.1", "1.0.1", "0.9.300.2.0",
-    "0.10-5", "R (>= 3.4.0)", "R (>= 3.4.0), stats (>= 3.2.5), parallel (>= 3.2.5)",
+    "lhs", "LVGP", "randtoolbox", "Rcpp", "RcppArmadillo", "rngWELL",
+    "1.0.1", "2.1.5", "1.17.1", "1.0.1", "0.9.300.2.0", "0.10-5",
+    "R (>= 3.4.0)", "R (>= 3.4.0), stats (>= 3.2.5), parallel (>= 3.2.5)",
     "rngWELL (>= 0.10-1)", "R (>= 3.0.0)", "R (>= 3.3.0)", "R (>= 3.0.0)",
-    NA, NA, NA, NA, NA, NA, "GPL-3", "GPL-2", "BSD_3_clause + file LICENSE",
-    "GPL (>= 2)", "GPL (>= 2)", "BSD_3_clause + file LICENSE", "i386, x64",
-    NA, "i386, x64", "i386, x64", "i386, x64", "i386, x64", "testthat, DoE.base, knitr, rmarkdown, covr",
-    NA, NA, "RUnit, inline, rbenchmark, knitr, rmarkdown, pinp, pkgKitten (>= 0.1.2)",
+    NA, NA, NA, NA, NA, NA,
+    "GPL-3", "GPL-2", "BSD_3_clause + file LICENSE",
+    "GPL (>= 2)", "GPL (>= 2)", "BSD_3_clause + file LICENSE",
+    "i386, x64", NA, "i386, x64", "i386, x64", "i386, x64", "i386, x64",
+    "testthat, DoE.base, knitr, rmarkdown, covr", NA, NA,
+    "RUnit, inline, rbenchmark, knitr, rmarkdown, pinp, pkgKitten (>= 0.1.2)",
     "RUnit, Matrix, pkgKitten, reticulate, rmarkdown, knitr, pinp, slam",
     NA, "Rcpp", "lhs(>= 0.14), randtoolbox(>= 1.17)", NA, "methods, utils",
-    "Rcpp (>= 0.11.0), stats, utils, methods", NA, "Rcpp", NA, NA,
-    NA, "Rcpp", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA
+    "Rcpp (>= 0.11.0), stats, utils, methods", NA, "Rcpp", NA, NA, NA, "Rcpp", NA,
+    NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA
   ),
   .Dim = c(6L, 11L),
-  .Dimnames = list(NULL, c("Package", "Version", "Depends", "Enhances", "License",
-                           "Archs", "Suggests", "Imports", "LinkingTo", "Priority", "License_is_FOSS")))
+  .Dimnames = list(NULL, c("Package", "Version", "Depends", "Enhances",
+                           "License", "Archs", "Suggests", "Imports",
+                           "LinkingTo", "Priority", "License_is_FOSS")))
   mock_write_packages(dir, type, db)
 }
 
@@ -98,30 +109,56 @@ for (pkg_type in names(types)) {
                            Rversion = rvers)
 
       restoreTestRepo(pkgsAdd)
-
-      # Delete only listed packages, leave all deps as is
+      # Test #1: delete only listed packages, leave all deps as is
       with_mock(
         download_packages = mock_download_packages,
         write_packages = mock_write_packages,
         .env = "miniCRAN",
         {
-          deletePackage(pkgsAdd, path = repo_root, type = pkg_type, Rversion = rvers, deps = FALSE)
+          deletePackage(pkgsAdd, path = repo_root, type = pkg_type,
+                        Rversion = rvers, deps = FALSE)
           pdb <- pkgAvail(repo_root,
                           type = pkg_type,
                           Rversion = rvers,
                           quiet = TRUE)[, "Package"]
         })
-
+      # Removed: RcppArmadillo, LVGP
+      # Remain: Rcpp, rngWELL, lhs, randtoolbox
       expect_true(
-        all(
-          c(!(pkgsAdd %in% pdb), pkgsDep1 %in% pdb,
-            pkgsDep2 %in% pdb, pkgsDep3 %in% pdb)
+        all(c(
+          !(pkgsAdd %in% pdb),
+          pkg_lhs %in% pdb, pkg_rtbox %in% pdb,
+          pkg_well %in% pdb, pkg_rcpp %in% pdb)
         )
       )
 
-      # Add the package once again, remove with all first-tier dependencies
       restoreTestRepo(pkgsAdd)
+      # Test #2: try deleting package that is required by others
+      with_mock(
+        download_packages = mock_download_packages,
+        write_packages = mock_write_packages,
+        .env = "miniCRAN",
+        {
+          deletePackage(pkg_lhs, path = repo_root, type = pkg_type, Rversion = rvers,
+                        deps = TRUE)
+          pdb <- pkgAvail(repo_root,
+                          type = pkg_type,
+                          Rversion = rvers,
+                          quiet = TRUE)[, "Package"]
+        })
+      # Removed: none
+      # Removed as deps: none
+      # Remain: RcppArmadillo, LVGP, Rcpp, rngWELL, lhs, randtoolbox
+      expect_true(
+        all(c(
+          pkgsAdd %in% pdb,
+          pkg_lhs %in% pdb, pkg_rtbox %in% pdb,
+          pkg_well %in% pdb, pkg_rcpp %in% pdb)
+        )
+      )
 
+      restoreTestRepo(pkgsAdd)
+      # Test #3: delete packages and first-tier dependencies
       with_mock(
         download_packages = mock_download_packages,
         write_packages = mock_write_packages,
@@ -134,17 +171,44 @@ for (pkg_type in names(types)) {
                           Rversion = rvers,
                           quiet = TRUE)[, "Package"]
         })
-
+      # Removed: RcppArmadillo, LVGP
+      # Removed as deps: Rcpp, lhs, randtoolbox
+      # Remain: rngWELL
       expect_true(
-        all(
-          c(!(pkgsAdd %in% pdb), !(pkgsDep1 %in% pdb),
-            pkgsDep2 %in% pdb, !(pkgsDep3 %in% pdb))
+        all(c(
+          !(pkgsAdd %in% pdb),
+          !(pkg_lhs %in% pdb), !(pkg_rtbox %in% pdb),
+          pkg_well %in% pdb, !(pkg_rcpp %in% pdb))
         )
       )
 
-      # Add the package once again, remove with all recursive dependencies
       restoreTestRepo(pkgsAdd)
+      # Test #4: delete packages and first-tier dependencies, but the dependency is required
+      with_mock(
+        download_packages = mock_download_packages,
+        write_packages = mock_write_packages,
+        .env = "miniCRAN",
+        {
+          deletePackage(pkg_rcpparma, path = repo_root, type = pkg_type, Rversion = rvers,
+                        deps = TRUE)
+          pdb <- pkgAvail(repo_root,
+                          type = pkg_type,
+                          Rversion = rvers,
+                          quiet = TRUE)[, "Package"]
+        })
+      # Removed: RcppArmadillo
+      # Removed as deps: none (Rcpp is held as required by others)
+      # Remain: LVGP, Rcpp, rngWELL, lhs, randtoolbox
+      expect_true(
+        all(c(
+          !(pkg_rcpparma %in% pdb), pkg_lvgp %in% pdb,
+          pkg_rtbox %in% pdb, pkg_lhs %in% pdb,
+          pkg_well %in% pdb, pkg_rcpp %in% pdb)
+        )
+      )
 
+      restoreTestRepo(pkgsAdd)
+      # Test #5: delete packages and recursive dependencies
       with_mock(
         download_packages = mock_download_packages,
         write_packages = mock_write_packages,
@@ -157,34 +221,39 @@ for (pkg_type in names(types)) {
                           Rversion = rvers,
                           quiet = TRUE)[, "Package"]
         })
-
+      # Removed: RcppArmadillo, LVGP
+      # Removed as deps: Rcpp, rngWELL, lhs, randtoolbox
+      # Remain: none
       expect_true(
-        all(
-          c(!(pkgsAdd %in% pdb), !(pkgsDep1 %in% pdb),
-            !(pkgsDep2 %in% pdb), !(pkgsDep3 %in% pdb))
+        all(c(
+          !(pkgsAdd %in% pdb),
+          !(pkg_rtbox %in% pdb), !(pkg_lhs %in% pdb),
+          !(pkg_well %in% pdb), !(pkg_rcpp %in% pdb))
         )
       )
 
-      # Add the package once again, remove with all reverse recursive dependencies
       restoreTestRepo(pkgsAdd)
-
+      # Test #6: delete packages and all reverse recursive dependencies
       with_mock(
         download_packages = mock_download_packages,
         write_packages = mock_write_packages,
         .env = "miniCRAN",
         {
-          deletePackage(pkgsDep3, path = repo_root, type = pkg_type, Rversion = rvers,
+          deletePackage(pkg_rcpp, path = repo_root, type = pkg_type, Rversion = rvers,
                         deps = TRUE, recursive = TRUE, reverse = TRUE)
           pdb <- pkgAvail(repo_root,
                           type = pkg_type,
                           Rversion = rvers,
                           quiet = TRUE)[, "Package"]
         })
-
+      # Removed: Rcpp
+      # Removed as revdeps (can't work without Rcpp): RcppArmadillo, LVGP, lhs
+      # Remain: rngWELL, randtoolbox
       expect_true(
-        all(
-          c(!(pkgsAdd %in% pdb),
-            pkgsDep2 %in% pdb, !(pkgsDep3 %in% pdb))
+        all(c(
+          !(pkgsAdd %in% pdb), !(pkg_rcpparma %in% pdb),
+          !(pkg_lvgp %in% pdb), !(pkg_lhs %in% pdb),
+          pkg_well %in% pdb, pkg_rtbox %in% pdb)
         )
       )
     })
